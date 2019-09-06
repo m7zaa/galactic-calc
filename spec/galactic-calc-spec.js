91045d31c459f4d76ec123fc7e3ae02caa3c0f27
Micah Zaayer <micahzaayer@gmail.com>
2019-09-06 10:15:49 -0700

    add working function to calculate age on mercury.

--- spec/galactic-calc-spec.js
+++ spec/galactic-calc-spec.js
@@ -2,20 +2,40 @@
 
 describe ('user input age valid', function() {
 
+//Pass
   it ("it should respond as 'false' if syntax is incorrect", function() {
     let myAge = userAge("three");
     expect(myAge).toEqual(false);
   });
+
+  //Pass
   it ("it should respond as 'true' if syntax is correct", function() {
     let myAge = userAge(33);
     expect(myAge).toEqual(true);
   });
 
 
+  it ("it should return age*0.24 to calculate age on Mercury", function() {
+    let myAgeMercury = mercuryAge(33);
+    expect(myAgeMercury).toEqual(7.92);
+  });
 
 
 
+  it ("it should return age*0.62 to calculate age on Venus", function() {
+    let myAgeVenus = venusAge(33);
+    expect(myAgeVenus).toEqual(20.46);
+  });
 
+  it ("it should return age*1.88 to calculate age on Mars", function() {
+    let myAgeMars = marsAge(33);
+    expect(myAgeMars).toEqual(62.04);
+  });
+
+  it ("it should return age*11.86 to calculate age on Jupiter", function() {
+    let myAgeJupiter = JupiterAge(33);
+    expect(myAgeJupiter).toEqual(391.38);
+  });
 
 
 
--- src/galactic-calc.js
+++ src/galactic-calc.js
@@ -1,9 +1,23 @@
 // Back-end
 
-export function userAge(input) {
+ function userAge(input) {
   if (input > 0 && input<1000000 || input=== NaN) {
         return true;
     } else {
       return false;
     }
-}
+};
+
+
+
+
+
+class Galactic {
+constructor() {
+  this.age = 33;
+  this.mercuryAge = []
+  }
+  mercuryCalc() {
+    this.mercuryAge = this.age * .24;
+  }
+};
