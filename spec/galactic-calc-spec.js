b78e6d869ba58e1a88281e84e2978a2cc62e5da2
Micah Zaayer <micahzaayer@gmail.com>
2019-09-06 09:48:21 -0700

    add two working tests for correct and incorrect user age input.

--- spec/galactic-calc-spec.js
+++ spec/galactic-calc-spec.js
@@ -2,11 +2,14 @@
 
 describe ('user input age valid', function() {
 
-  it ("it should respond as "undefined" if syntax is incorrect", function() {
+  it ("it should respond as 'false' if syntax is incorrect", function() {
     let myAge = userAge("three");
-    expect(myAge).toEqual("false");
+    expect(myAge).toEqual(false);
   });
-
+  it ("it should respond as 'true' if syntax is correct", function() {
+    let myAge = userAge(33);
+    expect(myAge).toEqual(true);
+  });
 
 
 
